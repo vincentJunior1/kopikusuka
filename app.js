f9d568c54c84d63022247090008db086a0c4061b
vincentjuniorrr <vincent_cars@yahoo.com>
2020-12-14 09:37:17 +0700

    Finish

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 const routesNavigation = require('./src/routesNavigation')
 
 app.use(morgan('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Request-With, Content-Type, Accept, Authorization'
+  )
+  next()
+})
 app.use('/', routesNavigation)
 app.get('*', (req, res) => {
   res.status(404).send('path not found')
--- src/config/mysql.js
+++ src/config/mysql.js
@@ -1,9 +1,10 @@
 const mysql = require('mysql')
+require('dotenv').config()
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'beginer_backend',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_DATABASE,
   timezone: 'UTC'
 })
 
